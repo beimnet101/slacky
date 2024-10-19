efedf43bf17d232ca44dc38fe186624495f38185
beimnet <beimnetworku101@gmail.com>
2024-10-19 13:24:42 +0300

    yes

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has TypeScript errors.
-      ignoreBuildErrors: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
